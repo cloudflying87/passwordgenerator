54ca990e01a9574d42c0122612f74a49ae3eb66f
unknown <davidhale87@gmail.com>
2020-06-11 17:34:14 -0500

    All of the prompts for the user have been completed. Length,Uppercase, lowercase, SpecialCharacters, Numbers. Have the lists of the characters done. Working on the logic of generating the password

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+//Variables
+var generateBtn
+var password, passwordText
+var passwordLength
+var upperCase, upperCaseChar, upperCaseSel
+var lowercase, lowerCaseChar, lowerCaseSel
+var special, specialChar, specialCharSel
+var num, NumChar, NumCharSel
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,14 +20,93 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+generateBtn.addEventListener("click", generatePass);
 generateBtn.addEventListener("click", writePassword);
 
 // User clicks the button to generate password. 
 // Listening for the click
+
 // Alert user for Password length and check that it is between 8-128
+function generatePass (){
+  getPassLength()
+  userLower()
+  userUpper()
+  userNumer()
+  userSpec()
+  conlog()
+}
+
+  function getPassLength (){
+    passwordLength = parseInt(prompt("Choose password lenght between 8-128"))
+    if (passwordLength<8){
+      getPassLength()
+    }
+    else if (passwordLength >128){
+      getPassLength()
+    }
+  }
 
 // Prompt user if they want lowercase letters
+function userLower(){
+  lowercase = confirm('Confirm you want lowercase letters.')
+}
+
 // Prompt user if they want uppercase letters
+function userUpper(){
+  upperCase = confirm('Confirm you want uppercase letters.')
+}
 // Prompt user if they want numeric characters
+function userNumer(){
+  num = confirm('Confirm you want numbers.')
+}
 // Prompt user if they want special characters
-// Generate random characters and letters based on user responses
+function userSpec (){
+  special = confirm('Confirm you want special characters.')
+}
+// Call random generators based on user responses for the length of the user specified password
+
+// for (var i = 0; i < passwordLength; i++){
+  upperCaseSel = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  lowerCaseSel = 'abcdefghijklmnopqrstuvwxyz'
+  specialCharSel = '~!@#$%^&*<>:'
+  NumCharSel = '0123456789'
+  if (upperCase){
+   var char = upperCaseSel
+  }
+  console.log(char)
+// }
+
+// Randomly generate uppercase letter
+function upperCaseFun(){
+  upperCaseSel = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  lowerCaseSel = 'abcdefghijklmnopqrstuvwxyz'
+  specialCharSel = '~!@#$%^&*<>:'
+  NumCharSel = '0123456789'
+  
+
+}
+// Randomly generate lowercase letters
+function lowerCaseFun(){
+
+}
+
+function conlog(){
+  console.log('Conlog')
+  console.log(passwordLength)
+  console.log(lowercase);
+  console.log(upperCase);
+  console.log(num);
+  console.log(special);
+
+}
+// function makeid(length) {
+//   var result           = '';
+//   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   var charactersLength = characters.length;
+//   for ( var i = 0; i < length; i++ ) {
+//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//   }
+//   return result;
+
+
+// console.log(makeid(5))
